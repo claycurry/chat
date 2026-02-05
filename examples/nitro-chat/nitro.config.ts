d414a4a6d412ff6e9672371bc3de20c2f679eba6
Rihan Arfan <me@file.properties>
2026-02-05 12:55:11 +0000

    chore(deps): upgrade dependencies

--- examples/nitro-chat/nitro.config.ts
+++ examples/nitro-chat/nitro.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from "nitro";
 
 export default defineConfig({
   serverDir: "./server",
-  compatibilityDate: "2025-01-20",
   traceDeps: [
-    "discord.js",
     "@discordjs/ws",
-    "@discordjs/voice",
-    "zlib-sync",
-    "tslib",
-    "@microsoft/microsoft-graph-client"
   ],
 });
